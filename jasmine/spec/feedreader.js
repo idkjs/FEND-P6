f9c2a341895a0c5ebb99c78221b868eb783b754a
Alain Armand <armand.github@gmail.com>
2016-05-12 19:13:54 -0400

    First Review Edits
    .done() call at the end is necessary for the test to run.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,17 +1,17 @@
-(function() {
-    'use strict'; 
-    $(function() {
+(function () {
+    'use strict';
+    $(function () {
         /* This is our first test suite - a test suite just contains
          * a related set of tests. This suite is all about the RSS
          * feeds definitions, the allFeeds variable in our application.
          */
-        describe('RSS Feeds', function() {
+        describe('RSS Feeds', function () {
             /* This is our first test - it tests to make sure that the
              * allFeeds variable has been defined and that it is not
              * empty. The second test, checks for the allFeed items 
              * have a defined, non null url.
              */
-            it('are defined', function() {
+            it('are defined', function () {
                 expect(allFeeds).toBeDefined();
                 expect(allFeeds.length).not.toBe(0);
             });
@@ -21,16 +21,16 @@
              * are not undefined or not empty strings.
              */
 
-            it('has url defined and the url field is not empty', function() {
-                allFeeds.forEach(function(feed) {
+            it('has url defined and the url field is not empty', function () {
+                allFeeds.forEach(function (feed) {
                     expect(feed.url).toBeDefined();
                     expect(feed.url).not.toBe(null);
                     expect(feed.url).not.toBe(' ');
                 });
             });
 
-            it('has name defined and the name field is not empty', function() {
-                allFeeds.forEach(function(feed) {
+            it('has name defined and the name field is not empty', function () {
+                allFeeds.forEach(function (feed) {
                     expect(feed.name).toBeDefined();
                     expect(feed.name).not.toBe(null);
                     expect(feed.name).not.toBe(' ');
@@ -39,11 +39,11 @@
         });
 
 
-        describe('The menu', function() {
+        describe('The menu', function () {
             /* This test ensures the menu element is
              * hidden by default.
              */
-            it('menu to be hidden by default', function() {
+            it('menu to be hidden by default', function () {
                 expect($('body').hasClass('menu-hidden')).toBe(true);
             });
 
@@ -51,7 +51,7 @@
              * visibility when the menu icon is clicked then closes when clicked
              * again.
              */
-            it('menu displays when clicked then hides when clicked again', function() {
+            it('menu displays when clicked then hides when clicked again', function () {
                 $('.menu-icon-link').click();
                 expect($('body').hasClass('menu-hidden')).toBeFalsy();
                 $('.menu-icon-link').click();
@@ -59,42 +59,42 @@
             });
         });
 
-        describe('Initial Entries', function() {
+        describe('Initial Entries', function () {
 
             /* This test ensures when the loadFeed function is called and
              * completes its work, there is at least
              * a single .entry element within the .feed container.
              */
-            beforeEach(function(done) {
+            beforeEach(function (done) {
                 loadFeed(0, done);
             });
 
-            it('should call and complete loadFeed with at least one item in the list', function() {
+            it('should call and complete loadFeed with at least one item in the list', function () {
                 var item = document.getElementsByClassName('entry');
                 expect(item.length).toBeGreaterThan(0);
             });
         });
 
-        describe('New Feed Selection', function() {
+        describe('New Feed Selection', function () {
             /* This test checks that when a new feed is loaded
              * by the loadFeed function that the content actually changes.
              */
 
             var oldTitles;
 
-            beforeEach(function(done) {
-                loadFeed(0, function() {
-                    oldTitles = $('.feed a article h2').map(function() {
+            beforeEach(function (done) {
+                loadFeed(0, function () {
+                    oldTitles = $('.feed a article h2').map(function () {
                         return $(this).text();
                     }).get();
                     done();
                 });
             });
 
-            it('old feed h2 title tags should not match new feed titles exactly', function(done) {
+            it('old feed h2 title tags should not match new feed titles exactly', function (done) {
 
-                loadFeed(1, function() {
-                    var newTitles = $('.feed a article h2').map(function() {
+                loadFeed(1, function () {
+                    var newTitles = $('.feed a article h2').map(function () {
                         return $(this).text();
                     }).get();
                     expect(oldTitles).not.toEqual(newTitles);
@@ -103,4 +103,4 @@
             });
         });
     });
-}());
+} ());
